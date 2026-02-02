c9f5bc49c26ccc432c6f40d0ae96acda2864a1e3
Alex <alexeysporykhin@gmail.com>
2026-02-02 11:32:00 +0300

    2.3*: Course information step 8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,14 +1,25 @@
 const Header = ({ courseName }) => <h1>{courseName}</h1>;
 
-const Content = ({ parts }) => parts.map((part) => <Part key={part.id} part={part} />);
+const Content = ({ parts }) =>
+  parts.map((part) => <Part key={part.id} part={part} />);
 
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>;
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0);
+  return <p>total of {totalExercises} exercises</p>;
+};
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header courseName={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
@@ -35,7 +46,7 @@ const App = () => {
       },
       {
         name: "My part by Alex",
-        exercises: 14000,
+        exercises: 100,
         id: 4,
       },
     ],
