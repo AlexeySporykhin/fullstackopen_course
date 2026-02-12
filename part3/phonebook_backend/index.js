481a9f17266f6d0d635ac366de4408b522d0d192
Alex <alexeysporykhin@gmail.com>
2026-02-12 10:35:34 +0300

    Configure Vite server proxy for API requests and remove CORS middleware from backend

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 var morgan = require('morgan')
-const cors = require('cors')
 const app = express()
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
