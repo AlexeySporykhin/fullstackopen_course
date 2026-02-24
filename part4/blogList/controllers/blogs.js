de977aa91f495e623cfd7067f67103a5919a1969
Alex <alexeysporykhin@gmail.com>
2026-02-24 13:22:09 +0300

    4.17: Blog List Expansion, step 5

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -12,12 +12,17 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).end()
   }
 
+  const user = await User.findOne({})
+
   const blog = new Blog({
     ...request.body,
-    likes: request.body.likes || 0
+    likes: request.body.likes || 0,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
   response.status(201).json(savedBlog)
 })
 
