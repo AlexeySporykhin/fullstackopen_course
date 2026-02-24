ef32e0ce69e7d8a82de49f607c1d21dc036585ea
Alex <alexeysporykhin@gmail.com>
2026-02-24 13:54:30 +0300

    4.19: Blog List Expansion, step 7
    Modified adding new blogs so that it is only possible if a valid token is sent with the HTTP POST request.

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,19 +1,36 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   if (!request.body.title || !request.body.url) {
     return response.status(400).end()
   }
 
-  const user = await User.findOne({})
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+  
   const blog = new Blog({
     ...request.body,
     likes: request.body.likes || 0,
@@ -23,6 +40,7 @@ blogsRouter.post('/', async (request, response) => {
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
+
   response.status(201).json(savedBlog)
 })
 
@@ -39,7 +57,7 @@ blogsRouter.put('/:id', async (request, response) => {
 
   updatedBlog.likes = request.body.likes || updatedBlog.likes
   const savedBlog = await updatedBlog.save()
-  
+
   return response.status(200).json(savedBlog)
 })
 
