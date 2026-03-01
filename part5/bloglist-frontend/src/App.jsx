365009a8953a093d7b193eb56b4298d2cd1657b6
Alex <alexeysporykhin@gmail.com>
2026-03-01 16:39:36 +0300

    5.10: Blog List Frontend, step 10 sorted the blog posts by the number of likes.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -131,7 +131,7 @@ const App = () => {
           createBlog={addBlog}
         />
       </Togglable>
-      {blogs.map(blog =>
+      {blogs.sort((a, b) => b.likes - a.likes).map(blog =>
         <Blog key={blog.id} blog={blog} updateBlog={updateBlog} />
       )}
     </div>
