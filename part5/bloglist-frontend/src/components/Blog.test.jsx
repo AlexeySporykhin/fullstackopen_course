0c418b355f8da2c5d9c340c1f03b9a617fc7115b
Alex <alexeysporykhin@gmail.com>
2026-03-02 13:18:58 +0300

    5.14: Blog List Tests, step 2 checks that the blog's URL and number of likes are shown when clicking the button

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
-import { beforeEach } from 'vitest'
+import { beforeEach, expect } from 'vitest'
+import userEvent from '@testing-library/user-event'
 
-describe('', () => {
+describe('renders blog\'s content', () => {
   beforeEach(() => {
     const blog = {
       title: 'test title',
@@ -26,5 +27,35 @@ describe('', () => {
   test('doesnt render url', () => {
     const element = screen.getByText('url-test.com')
     expect(element).not.toBeVisible()
+  })
+})
+
+describe('clicking the button view displays the url and likes', () => {
+  beforeEach(() => {
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      likes: 5,
+      url: 'url-test.com'
+    }
+
+    render(<Blog blog={blog} />)
+  })
+  test('renders likes', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const element = screen.getByText('5', { exact: false })
+    expect(element).toBeVisible()
+  })
+
+  test('renders url', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const element = screen.getByText('url-test.com')
+    expect(element).toBeVisible()
   })
 })
