625d9d2d82c0948be1e4cbe673f1aa048ddad6bc
Alex <alexeysporykhin@gmail.com>
2026-03-02 12:51:48 +0300

    5.13: Blog List Tests, step 1 updated

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,46 +1,30 @@
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
+import { beforeEach } from 'vitest'
 
-test('renders title and author', () => {
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    likes: 5,
-    url: 'url-test.com'
-  }
+describe('', () => {
+  beforeEach(() => {
+    const blog = {
+      title: 'test title',
+      author: 'test author',
+      likes: 5,
+      url: 'url-test.com'
+    }
 
-  render(<Blog blog={blog} />)
+    render(<Blog blog={blog} />)
+  })
+  test('renders title and author', () => {
+    const element = screen.getByText('test title test author')
+    expect(element).toBeVisible()
+  })
 
-  const element = screen.getByText('test title test author')
-  expect(element).toBeVisible()
-})
+  test('doesnt render likes', () => {
+    const element = screen.getByText('5', { exact: false })
+    expect(element).not.toBeVisible()
+  })
 
-test('doesnt render likes', () => {
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    likes: 5,
-    url: 'url-test.com'
-  }
-
-  render(<Blog blog={blog} />)
-  screen.debug()
-
-  const element = screen.getByText('5', { exact: false })
-  expect(element).not.toBeVisible()
-})
-
-test('doesnt render url', () => {
-  const blog = {
-    title: 'test title',
-    author: 'test author',
-    likes: 5,
-    url: 'url-test.com'
-  }
-
-  render(<Blog blog={blog} />)
-  screen.debug()
-
-  const element = screen.getByText('url-test.com')
-  expect(element).not.toBeVisible()
+  test('doesnt render url', () => {
+    const element = screen.getByText('url-test.com')
+    expect(element).not.toBeVisible()
+  })
 })
