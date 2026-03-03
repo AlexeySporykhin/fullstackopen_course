766cd432f80e29692b6d3c23c3ea078024af502b
Alex <alexeysporykhin@gmail.com>
2026-03-03 17:48:06 +0300

    5.18: Blog List End To End Testing, step 2 Test fot successful and failed login

--- part5/blogs-e2e/tests/blog_app.spec.js
+++ part5/blogs-e2e/tests/blog_app.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        username: 'alexsp',
+        name: 'Alexey Sp',
+        password: 'test'
+      }
+    })
     await page.goto('/')
   })
 
@@ -11,5 +19,23 @@ describe('Blog app', () => {
     await expect(page.getByLabel('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
   })    
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('alexsp')
+      await page.getByLabel('password').fill('test')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Alexey Sp logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('username').fill('alexsp')
+      await page.getByLabel('password').fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('Wrong username or password')).toBeVisible()
+    })
+  })
 })
 
