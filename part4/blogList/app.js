ec4118ae296abec24a2e3c01bb1a12576797da7a
Alex <alexeysporykhin@gmail.com>
2026-03-03 17:39:11 +0300

    commit before tasks added helper enviroment  before making tasks

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -12,22 +12,27 @@ const app = express()
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-.connect(config.MONGODB_URI, { family: 4 })
-.then(() => {
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
     logger.info('connected to MongoDB')
-})
-.catch((error) => {
+  })
+  .catch((error) => {
     logger.error('error connection to MongoDB:', error.message)
-})
+  })
 
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use(middleware.tokenExtractor)
-app.use('/api/blogs', middleware.userExtractor, blogsRouter)
-app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
