82f2e7c75d0768231d4a0b6ca71d5dc1038d923d
Alex <alexeysporykhin@gmail.com>
2026-03-06 16:36:18 +0300

    6.15 Anecdotes and the Backend, step 2
    implement anecdote creation with backend integration

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,19 +1,16 @@
 import { createSlice, current } from '@reduxjs/toolkit'
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({ content, id: generateId(), votes: 0 })
+      state.push(action.payload)
     },
     handleVote(state, action) {
       const id = action.payload
       console.log('id', id)
-      
+
       const anecdoteToChange = state.find(an => an.id === id)
       console.log('anecdoteToChange', current(anecdoteToChange))
       const changedAnecdote = {
@@ -24,7 +21,7 @@ const anecdoteSlice = createSlice({
       return state.map(an => (an.id !== id ? an : changedAnecdote))
     },
     setAnecdotes(state, action) {
-      return action.payload 
+      return action.payload
     }
   }
 })
