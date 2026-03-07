c490e72ba1c8aa41801b0397c22633bd6939bdc7
Alex <alexeysporykhin@gmail.com>
2026-03-07 18:20:23 +0300

    Exercise 6.21 Implement adding new anecdotes to the server using React Query.

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -5,6 +5,7 @@ import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  
   const handleVote = (anecdote) => {
     console.log('vote')
   }
