94de8888dc90c31d17e73a685306bc7e81b5cdd8
Alex <alexeysporykhin@gmail.com>
2026-03-11 10:13:38 +0300

    7.4: Anecdotes and Hooks step 1
    Simplify the anecdote creation form of your application with the useField

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Routes, Route, Link, useMatch, useNavigate } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -31,7 +32,7 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map(anecdote => <li key={anecdote.id} >
-      <Link to={`/anecdotes/${anecdote.id}`}>  {anecdote.content}</Link>
+        <Link to={`/anecdotes/${anecdote.id}`}>  {anecdote.content}</Link>
       </li>
       )}
     </ul>
@@ -61,21 +62,20 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
-
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    navigate('/')    
+    navigate('/')
   }
 
   return (
@@ -84,15 +84,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -102,7 +102,7 @@ const CreateNew = (props) => {
 }
 
 const Notification = (props) => {
-  return props.message ? <div>{props.message}</div> : null  
+  return props.message ? <div>{props.message}</div> : null
 }
 
 const App = () => {
@@ -154,7 +154,7 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <Notification message = {notification}/>
+      <Notification message={notification} />
       <Routes>
         <Route path="/anecdotes/:id" element={<Anecdote anecdote={anecdote} />} />
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
