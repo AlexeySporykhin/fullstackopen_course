b39d8a7d208a97a51ed5844098925f0d79d2da89
Alex <alexeysporykhin@gmail.com>
2026-03-15 08:09:47 +0300

    Add Redux store configuration and update dependencies

--- part5/bloglist-frontend/src/main.jsx
+++ part5/bloglist-frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import App from './App'
-import notificationReducer from './reducers/notificationReducer'
+import store from './store'
 
-const store = createStore(notificationReducer)
+store.subscribe(() => console.log(store.getState()))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
