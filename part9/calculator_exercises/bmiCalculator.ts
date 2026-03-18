ccea6ff91a78ecf6691c049c2d2ae84c25d4f8a8
Alex <alexeysporykhin@gmail.com>
2026-03-18 11:39:11 +0300

    9.1 Body mass index Add BMI calculator

---
+++ part9/calculator_exercises/bmiCalculator.ts
@@ -0,0 +1,13 @@
+const calculateBmi = (height: number, weight: number) => {
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    if (bmi < 18.5) {
+        return 'Underweight';
+    } else if (bmi < 25) {
+        return 'Normal range';
+    } else {
+        return 'Overweight';
+    }
+}
+
+console.log(calculateBmi(180, 74));
