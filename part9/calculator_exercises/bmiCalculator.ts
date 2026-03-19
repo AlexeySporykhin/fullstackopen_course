00d4c838eb97dc2262f44eef38f444070ed14914
Alex <alexeysporykhin@gmail.com>
2026-03-19 09:42:10 +0300

    9.5 WebBMI

--- part9/calculator_exercises/bmiCalculator.ts
+++ part9/calculator_exercises/bmiCalculator.ts
@@ -5,20 +5,20 @@ interface MultiplyValues {
   weight: number;
 }
 const parseArguments = (args: string[]): MultiplyValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
 
   if (!isNotNumber(args[2]) && !isNotNumber(args[3])) {
     return {
       height: Number(args[2]),
-      weight: Number(args[3])
-    }
+      weight: Number(args[3]),
+    };
   } else {
-    throw new Error('Provided values were not numbers!');
+    throw new Error("Provided values were not numbers!");
   }
-}
+};
 
-const calculateBmi = (height: number, weight: number) => {
+export const calculateBmi = (height: number, weight: number) => {
   const heightInMeters = height / 100;
   const bmi = weight / (heightInMeters * heightInMeters);
   if (bmi < 18.5) {
@@ -30,13 +30,15 @@ const calculateBmi = (height: number, weight: number) => {
   }
 };
 
-try {
-    const {height, weight} = parseArguments(process.argv)
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
     console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
     if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
+      errorMessage += " Error: " + error.message;
     }
     console.log(errorMessage);
+  }
 }
--- part9/calculator_exercises/index.ts
+++ part9/calculator_exercises/index.ts
@@ -1,9 +1,26 @@
-import express from 'express';
+import express from "express";
+import qs from "qs";
+import { calculateBmi } from "./bmiCalculator";
+import { isNotNumber } from "./utils/helperFunctions";
 
 const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get("/hello", (_req, res) => {
+  return res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const { height, weight } = qs.parse(req.url.split("?")[1]);
+  if (!height || !weight || isNotNumber(height) || isNotNumber(weight)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+  const bmi = calculateBmi(Number(height), Number(weight));
+
+  return res.json({
+    height,
+    weight,
+    bmi,
+  });
 });
 
 const PORT = 3003;
