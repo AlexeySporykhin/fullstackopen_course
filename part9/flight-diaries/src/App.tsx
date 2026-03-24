6198f4cea8d6e7445cc64c3b33ae0a780d8b4794
Alex <alexeysporykhin@gmail.com>
2026-03-24 06:39:12 +0300

    Exercise 9.18 Make it possible to add new diary entries from the frontend.

--- part9/flight-diaries/src/App.tsx
+++ part9/flight-diaries/src/App.tsx
@@ -1,23 +1,58 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
-import { apiBaseUrl } from "./constants";
-import { DiaryEntry } from "./types";
+import { DiaryEntry, DiaryVisibility, DiaryWeather, NewDiaryEntry } from "./types";
+import { createDiaryEntry, getAllDiaries } from "./services/diaries";
 
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState<string>("");
+  const [visibility, setVisibility] = useState<string>("");
+  const [weather, setWeather] = useState<string>("");
   useEffect(() => {
-    axios.get<DiaryEntry[]>(`${apiBaseUrl}/diaries`).then(res => {
-      setDiaries(res.data);
+    getAllDiaries().then(data => {
+      setDiaries(data);
     });
   }, []);
+
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const newDiary: NewDiaryEntry = {
+      date: date,
+      visibility: visibility as DiaryVisibility,
+      weather: weather as DiaryWeather,
+      comment: "",
+    };
+    createDiaryEntry(newDiary).then(data => {
+      setDiaries([...diaries, data]);
+    });
+    setDate("");
+    setVisibility("");
+    setWeather("");
+  };
+
   return (
     <div>
+      <h2> Add new entry</h2>
+      <form onSubmit={diaryCreation} style={{ marginBottom: "20px" }}>
+        <div>
+          <label htmlFor="date">Date</label>
+          <input name="date" id="date" placeholder="YYYY-MM-DD" value={date} onChange={({target}) => setDate(target.value)} />
+        </div>
+        <div>
+          <label htmlFor="visibility">Visibility</label>
+          <input name="visibility" id="visibility" placeholder="Visibility" value={visibility} onChange={({target}) => setVisibility(target.value)} />
+        </div>
+        <div>
+          <label htmlFor="weather">Weather</label>
+          <input name="weather" id="weather" placeholder="Weather" value={weather} onChange={({target}) => setWeather(target.value)} />
+        </div>
+        <button type="submit">Add</button>
+      </form>
       <h2> Diary entries</h2>
       {diaries.map(diary => (
         <div key={diary.id}>
-        <h3>{diary.date}</h3>
-        <p>visibility: {diary.visibility}</p>
-        <p>weather: {diary.weather}</p>
+          <h3>{diary.date}</h3>
+          <p>visibility: {diary.visibility}</p>
+          <p>weather: {diary.weather}</p>
         </div>
       ))}
     </div>
---
+++ part9/flight-diaries/src/services/diaries.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { apiBaseUrl } from "../constants";
+import { DiaryEntry, NewDiaryEntry } from "../types";
+
+export const getAllDiaries = async () => {
+  const { data } = await axios.get<DiaryEntry[]>(`${apiBaseUrl}/diaries`);
+  return data;
+};
+
+export const createDiaryEntry = async (object: NewDiaryEntry) => {
+  console.log('object:', object);
+  const { data } = await axios.post<DiaryEntry>(`${apiBaseUrl}/diaries`, object);
+  return data;
+};
